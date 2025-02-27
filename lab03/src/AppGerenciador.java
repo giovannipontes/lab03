acb104534da835836f8892897397ba0bb467e2dc
giovannipontes <118862566+giovannipontes@users.noreply.github.com>
2025-02-27 00:00:18 +0000

    Criar Projeto lab03

---
+++ lab03/src/AppGerenciador.java
@@ -0,0 +1,7 @@
+public class AppGerenciador {
+    public static void main(String[] args) {
+        GerenciadorNomes gn = new GerenciadorNomesMem();
+        Ihm ihm = new Ihm(gn);
+        ihm.dialogar();
+    }
+}
